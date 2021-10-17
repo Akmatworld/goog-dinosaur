fa1ead0b67f8f6787b6bbcc8dc29f729740252e8
Akmat <sirazhidinov.akmat@tftc.dev>
2021-10-17 20:37:51 +0600

    Added paddle for ping pong game

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ window.onload = () => {
 	let dx = 2;
 	let dy = -2;
 	let ballRadius = 10;
+	let paddleHeight = 10;
+	let paddleWidth = 75;
+	let paddleX = (canvas.width-paddleWidth) / 2;
+	let rightPressed = false;
+	let leftPressed = false;
+
+
+	document.addEventListener("keydown", keyDownHandler, false);
+	document.addEventListener("keyup", keyUpHandler, false);
+
+	function keyDownHandler(e) {
+	    if(e.key == "Right" || e.key == "ArrowRight") {
+	        rightPressed = true;
+	    }
+	    else if(e.key == "Left" || e.key == "ArrowLeft") {
+	        leftPressed = true;
+	    }
+	}
+
+	function keyUpHandler(e) {
+	    if(e.key == "Right" || e.key == "ArrowRight") {
+	        rightPressed = false;
+	    }
+	    else if(e.key == "Left" || e.key == "ArrowLeft") {
+	        leftPressed = false;
+	    }
+	}
 
 
 	function drawBall() {
@@ -16,15 +43,40 @@ window.onload = () => {
 	    ctx.closePath();
 	}
 
+	function drawPaddle() {
+	    ctx.beginPath();
+	    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	    ctx.fillStyle = "#0095DD";
+	    ctx.fill();
+	    ctx.closePath();
+	}
+
+
 	function draw() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		drawBall();
+		drawPaddle();
+
 		if(y + dy > canvas.height - ballRadius || y + dy < 0) {
 		    dy = -dy;
 		}
+
 		if(x + dx > canvas.width - ballRadius || x + dx < 0) {
 		    dx = -dx;
 		}
+
+		if (leftPressed) {
+			paddleX -= 7;
+			if (paddleX < 0){
+		        paddleX = 0;
+		    }
+		}else if (rightPressed) {
+			paddleX += 7;
+			if (paddleX + paddleWidth > canvas.width){
+		        paddleX = canvas.width - paddleWidth;
+		    }
+		}
+
 		x += dx;
 		y += dy;
 	}
